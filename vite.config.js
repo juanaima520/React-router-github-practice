1c83bb81ed6fb7527f310784576830fa60c44cbb
Yajuan <juanaima520@gmail.com>
2024-10-15 20:39:48 -0500

    test main branch

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-router-github-practice",
   plugins: [react()],
   server: {
     port: 3005,
